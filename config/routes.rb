de45d110dff536f523471c01b46c63c2f70d519b
HarronB <bharron@academic.rrc.ca>
2019-03-07 14:33:37 -0600

    Migrated tables to DB. Created controller for Team model with index and show actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'teams/index'
+  get 'teams/show'
   # restful routes, only need index and show
   resources :teams, only: %i[index show]
   resources :players, only: %i[index show]
